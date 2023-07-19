bccbff582c7096ffedeb4fffcee66ce39ea8e9b8
pedroog304 <pedroog304@gmail.com>
2023-07-19 17:17:15 -0600

    tablar para mostrar a los empleados registrados

---
+++ view/mostrarEmpleados.php
@@ -0,0 +1,29 @@
+<?php include('header.php'); ?>
+
+<main class="container p-4">
+  <div class="row">
+    <div class="col-md-12">
+    <div class="col-md-6">
+      <a id="btn-inicio" href="" class="btn btn-success btn-block" value="Volver a la pagina principal">Volver a movimientos</a>
+      </div>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Numero</th>
+            <th>Nombre</th>
+            <th>Rol</th>
+            <th>Editar / Eliminar</th>
+          </tr>
+        </thead>
+        <tbody id="myTable">
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</main>
+<?php include('footer.php'); ?>
